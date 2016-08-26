221134c942fe117f65b55168f4ba62e654ee06e7
Sainath Adapa <adapasainath@gmail.com>
2016-08-26 12:54:13 +0530

    glimpse_output_as_org_table

---
+++ glimpse_output_as_org_table.R
@@ -0,0 +1,6 @@
+glimpse_output_as_org_table <- function(x) {
+  capture.output(glimpse(x)) %>%
+    str_replace_all('^\\$', '| ') %>% 
+    str_replace_all('(.*)<(.*)>(.*)', '\\1|<\\2>|\\3 |') %>% 
+    paste0(collapse = '\n') %>% cat  
+}
